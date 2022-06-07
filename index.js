96b22c754db3606929d8b0746314e6f94424ede0
George Duncan <gtduncan@usc.edu>
2022-06-06 23:09:05 -0700

    commit tree 362d13c0d336a4aa2391dfb8d5d07bb0a76c1e03

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name : "George",
+    streetAddress : "1208 West 25th"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value)
+{
+    const newObject = {...employee}
+    newObject[key] = value;
+    return newObject;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+{
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key)
+{
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key)
+{
+    delete employee[key];
+    return employee;
+}
